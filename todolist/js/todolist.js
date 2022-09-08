dda4746a5998f5023cf6c9b65ed4d6585cce17f7
rtardo <84642031+rtardo@users.noreply.github.com>
2022-09-09 01:37:02 +0300

    To-Do List App from patika.dev

--- todolist/js/todolist.js
+++ todolist/js/todolist.js
@@ -1,16 +1,82 @@
 'use strict';
 
-let listItems = document.getElementsByTagName("li");
+let list = document.getElementById('list');
+let addButton = document.getElementById('liveToastBtn');
 
-for(let i=0; i < listItems.length; i++){ 
-    let closeButton = document.createElement("span"); //close icon'u span etiketi içersinde olduğu için yeni bir span elemanı oluşturup ve closeButton değişkenine atadık.
-    closeButton.textContent = "\u00D7"; // listede çarpı işaretini oluşturabilmek için "\u00D7" kullandık.
-    closeButton.classList.add("close"); // bu butona close class'ını ekledik.
-    //closeButton.onclick = removeButton; // ve çarpı işaretine basınca removeButton fonsiyonunu çalıştırması gerektini söyledik.
-    listItems[i].appendChild(closeButton); // closeButton değişkenini ullength'in 0, 1, 2... indexlerine ekleyerek aslında for döngüsünü kullanarak var olan listeye çarpı butonunu ekledik. 
-    //listItems[i].onclick = check; // üzerine tıklayınca check fonksiyonunu çalıştır dedik.
+addButton.addEventListener('click', addNewElement);
+window.addEventListener("load", load);
+
+
+function load() {
+    let memory = JSON.parse(localStorage.getItem("task"));
+    if (memory) {
+        list.innerHTML = memory;
+
+        let listElemens = document.getElementsByClassName('list-item');
+        addEvent(listElemens, markListItem);
+
+        let closeButtons = document.getElementsByClassName('close');
+        addEvent(closeButtons, removeListItem)
+
+    }
 }
 
-console.log(listItems);
+function addEvent(items, callback, type = "click") {
+    for(let item of items) {
+        item.addEventListener(type, callback);
+    }
+}
+//Marks or unmarks list element
+function markListItem(event) {
+    event.target.classList.toggle('checked');
+    let backgroundColor = event.target.classList.contains('checked') ? 'rgb(0, 255, 0)' : 'rgb(255, 255, 255)';
+    event.target.style.backgroundColor = backgroundColor;
+    saveList(list);
+}
 
-function newElement() {}
+function removeListItem(event) {
+    event.target.parentElement.remove();
+    saveList(list);
+}
+
+//Adds list element on page if input not null
+function addNewElement(event) {
+    let inputValue = document.getElementById('task').value;
+    if (inputValue.trim().length > 0) {
+        const listItem = createListItem(inputValue);
+        list.appendChild(listItem);
+        inputValue = '';
+        $('#successToast').toast('show')
+    }
+    else {
+        $('#errorToast').toast('show')
+    }
+
+    saveList(list);
+}
+
+function createListItem(inputValue) {
+    const listElement = document.createElement("li");
+    listElement.textContent = inputValue;
+    listElement.classList.add("list-item");
+
+    const closeButton = createCloseButton();
+    listElement.append(closeButton);
+
+    listElement.addEventListener("click", markListItem);
+    listElement.firstElementChild.addEventListener("click", removeListItem);
+
+    return listElement;
+}
+
+function createCloseButton() {
+    const closeButton = document.createElement("span");
+    closeButton.classList.add("close");
+    closeButton.textContent = "\u00D7";
+    return closeButton;
+}
+
+//Saves list to Local Storage
+function saveList(list) {
+    localStorage.setItem("task", JSON.stringify(list.innerHTML));
+}
